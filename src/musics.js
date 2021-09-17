93bc675b32ee08d3ebc32c2a085f05694afd047d
ashkan31 <ashkan.matae@yahoo.com>
2021-09-17 15:25:32 +0430

    add project

---
+++ src/musics.js
@@ -0,0 +1,17 @@
+export default [
+  {
+    source: './musics/Avaz Nemishi.mp3',
+    photo: './musics/avaz-nemishi.jpg',
+    title: 'Avaz Nemishi'
+  },
+  {
+    source: './musics/Yakh Zadam.mp3',
+    photo: './musics/yakh-zadam.jpg',
+    title: 'Yakh Zadam'
+  },
+  {
+    source: './musics/Baroon Delam Khast.mp3',
+    photo: './musics/Baroon-Delam-Khast.jpg',
+    title: 'Baroon Delam Khast'
+  }
+];
